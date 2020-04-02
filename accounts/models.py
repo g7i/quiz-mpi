c4c5c0f0aa392cab88a8d9f5eeb1a5e7bea6b307
GouravSaini125 <rahul777singh000@gmail.com>
2020-04-02 21:17:45 +0530

    IC4

--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,8 @@ from quizApp.models import Quiz
 
 
 class Attempted(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    quiz = models.ForeignKey(Quiz, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     got = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
 
