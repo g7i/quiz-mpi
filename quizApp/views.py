54f93754d6d0ee9aee0e237b88d129e90391a4f9
GouravSaini125 <rahul777singh000@gmail.com>
2020-04-03 16:25:12 +0530

    IC6

--- quizApp/views.py
+++ quizApp/views.py
@@ -9,7 +9,7 @@ from .models import Quiz
 def index(request):
     quizzes = list(Quiz.objects.filter(is_active=True).filter(subject=request.user.last_name).order_by('-created'))
     attempted = list(Attempted.objects.filter(user=request.user).values_list('quiz', flat=True))
-    for quiz in Quiz.objects.filter(is_active=True).order_by('-created'):
+    for quiz in Quiz.objects.filter(is_active=True).filter(subject=request.user.last_name).order_by('-created'):
         if quiz.id in attempted:
             quizzes.remove(quiz)
     context = {
